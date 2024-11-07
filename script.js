06c2eabd382fc877031b1d13abbed3e16b1dd426
nyoung90 <musicislovellc@gmail.com>
2024-11-07 05:13:45 +0000

    added JS

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// all code for this lab to be completed in this file
+
+let currentTemperature = 25;
+
+if (currentTemperature > 32) {
+    console.log("It is not freezing.")
+} else {
+    console.log("It is freezing.")
+}
+
+
+let age1 = 35
+let age2 = 65
+
+if (age2 > age1) {
+    console.log("I am older than you!") //older
+} else if (age2 < age1) {
+    console.log("Youth wins!") //younger
+} else if (age1 === age2) {
+    console.log("We are the same age!") //same age
+} else {
+    console.log("wrong info")
+}
+
+let isLoggedIn = false;
+
+if (isLoggedIn === true) {
+    console.log("Welcome Back!")
+} else {
+    console.log("Please log back in.")
+}
+   
+    
